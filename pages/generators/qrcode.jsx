16806363e4fab8de3a17db97db5c7e8b988e5767
Arpan Pandey <arpanpandey.aps@gmail.com>
2022-02-10 14:41:10 +0530

    Improved some errors

--- pages/generators/qrcode.jsx
+++ pages/generators/qrcode.jsx
@@ -51,9 +51,9 @@ export default function QRCode() {
   }
 
   return (
-    <section class="text-gray-400 bg-gray-900 body-font relative">
-      <div class="container px-5 py-24 mx-auto flex sm:flex-nowrap flex-wrap">
-        <div class="lg:w-2/3 md:w-1/2 bg-gray-900 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
+    <section className="text-gray-400 bg-gray-900 body-font relative">
+      <div className="container px-5 py-24 mx-auto flex sm:flex-nowrap flex-wrap">
+        <div className="lg:w-2/3 md:w-1/2 bg-gray-900 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
           <QRImage
             text={value}
             options={{
@@ -71,26 +71,26 @@ export default function QRCode() {
             className="absolute inset-0 w-full h-full"
           />
         </div>
-        <div class="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
-          <h2 class="text-white text-lg mb-1 font-medium title-font">
+        <div className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
+          <h2 className="text-white text-lg mb-1 font-medium title-font">
             Generate QR Code
           </h2>
-          <p class="leading-relaxed mb-5">
+          <p className="leading-relaxed mb-5">
             Enter the text you want to generate a QR code for.
           </p>
-          <div class="relative mb-4">
-            <label for="message" class="leading-7 text-sm text-gray-400">
+          <div className="relative mb-4">
+            <label for="message" className="leading-7 text-sm text-gray-400">
               Message
             </label>
             <textarea
               id="message"
               name="message"
               onChange={handleChange}
-              class="w-full bg-gray-800 rounded border border-gray-700 focus:border-yellow-500 focus:ring-2 focus:ring-yellow-900 h-32 text-base outline-none text-gray-100 py-1 px-3 reFrontColor-none leading-6 transition-colors duration-200 ease-in-out"
+              className="w-full bg-gray-800 rounded border border-gray-700 focus:border-yellow-500 focus:ring-2 focus:ring-yellow-900 h-32 text-base outline-none text-gray-100 py-1 px-3 reFrontColor-none leading-6 transition-colors duration-200 ease-in-out"
             ></textarea>
           </div>
-          <div class="relative mb-4">
-            <label for="FrontColor" class="leading-7 text-sm text-gray-400">
+          <div className="relative mb-4">
+            <label for="FrontColor" className="leading-7 text-sm text-gray-400">
               Front Color (Without #)
             </label>
             <input
@@ -98,11 +98,11 @@ export default function QRCode() {
               name="FrontColor"
               type="text"
               onClick={handleFrontColor}
-              class="w-full bg-gray-800 rounded border border-gray-700 focus:border-yellow-500 focus:ring-2 focus:ring-yellow-900 h-16 outline-none text-gray-100 py-1 px-3 reFrontColor-none leading-6 transition-colors duration-200 ease-in-out"
+              className="w-full bg-gray-800 rounded border border-gray-700 focus:border-yellow-500 focus:ring-2 focus:ring-yellow-900 h-16 outline-none text-gray-100 py-1 px-3 reFrontColor-none leading-6 transition-colors duration-200 ease-in-out"
             ></input>
           </div>
-          <div class="relative mb-4">
-            <label for="BackColor" class="leading-7 text-sm text-gray-400">
+          <div className="relative mb-4">
+            <label for="BackColor" className="leading-7 text-sm text-gray-400">
               Back Color (Without #)
             </label>
             <input
@@ -110,10 +110,10 @@ export default function QRCode() {
               name="BackColor"
               type="text"
               onClick={handleBackColor}
-              class="w-full bg-gray-800 rounded border border-gray-700 focus:border-yellow-500 focus:ring-2 focus:ring-yellow-900 h-16 outline-none text-gray-100 py-1 px-3 reFrontColor-none leading-6 transition-colors duration-200 ease-in-out"
+              className="w-full bg-gray-800 rounded border border-gray-700 focus:border-yellow-500 focus:ring-2 focus:ring-yellow-900 h-16 outline-none text-gray-100 py-1 px-3 reFrontColor-none leading-6 transition-colors duration-200 ease-in-out"
             ></input>
           </div>
-          <p class="text-xs text-yellow-300 text-opacity-90 mt-3">
+          <p className="text-xs text-yellow-300 text-opacity-90 mt-3">
             Note:- This QR code is generated from the text you have entered. We
             don't store any data. We don't know what is the text you have
             entered so we have no liability over anything generated using this
